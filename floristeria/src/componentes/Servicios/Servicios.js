979fcbf44601756954f99dea2b553b5b61a306ab
Leisny <leisny.moreno.6445@miremington.edu.co>
2023-11-28 23:31:28 -0500

    cuarto componente

---
+++ floristeria/src/componentes/Servicios/Servicios.js
@@ -0,0 +1,14 @@
+import './Servicios.css';
+import React from 'react';
+
+function Servicios(){
+    return(
+        <div className="servicios">
+            <div className="e"><a href="#">Rastrea tu Pedido</a></div>
+            <div className="e"><a href="#">Contactenos</a></div>
+            <div className="e"><a href="#">PQR</a></div>
+        </div>
+    );
+}
+
+export default Servicios;
