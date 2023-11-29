7feb7126efdb4535bcf0340be4086f8c70f1274d
Leisny <leisny.moreno.6445@miremington.edu.co>
2023-11-28 23:19:44 -0500

    primer componente encabezado

---
+++ floristeria/src/componentes/Encabezado/Encabezado.js
@@ -0,0 +1,12 @@
+import './Encabezado.css';
+import React from 'react';
+
+function Encabezado(){
+    return(
+        <div className="titulo">
+            <h1 className="nombre"> La Floristeria DA&LE</h1>
+        </div>
+    );
+}
+
+export default Encabezado;
