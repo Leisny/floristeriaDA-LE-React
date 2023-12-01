7ccc8d41c8e1a2b5beddeab9704da85ad538f60f
davinsonpympy <davinson.palacio.1306@miremington.edu.co>
2023-11-30 19:50:44 -0500

    Modificaciones en el componnente Opciones

--- floristeria/src/componentes/Opciones/Opciones.js
+++ floristeria/src/componentes/Opciones/Opciones.js
@@ -1,7 +1,10 @@
 import './Opciones.css';
-import React from 'react';
+import React, { useState } from 'react';
+import IniciarSeccion from '../iniciar-Seccion/IniciarSeccion';
 
 function Opciones(){
+
+    const[estadoModal1, cambiarModal1] = useState(false);
     return(
         <div className="opciones">
             <div className="a"><a href="#">Catálogo de Productos</a></div>
@@ -9,6 +12,24 @@ function Opciones(){
             <div className="a"><a href="#">Inventario</a></div>
             <div className="a"><a href="#">Preferencias de Arreglos</a></div>
             <div className="a"><a href="#">Seguimiento y Confirmación de entregas</a></div>
+            <div className='a'>
+                <a href='#' onClick={()=> cambiarModal1(!estadoModal1)}>Iniciar Sesión</a>
+                <IniciarSeccion 
+                    estado={estadoModal1}
+                    cambiarEstado={cambiarModal1}
+                    id="login-form"
+                >
+                    <div className='Contenido'>
+                        <h1>Iniciar Sección</h1>
+                        <labe className="label" for="username">Usuario</labe>
+                        <input className='input' type='text' id="username" placeholder='Ingresa tu Usuario'/>
+                        <labe className="label" for="password">Contraseña</labe>
+                        <input className='input' type='password' id="password" placeholder='Ingresa tu Contraseña'/>
+                        <h2><button >Iniciar</button></h2> 
+                        <h2><button >Registrarse</button></h2> 
+                    </div>
+                </IniciarSeccion>  
+            </div>
         </div>
     );
 }
